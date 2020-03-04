7e113d1c6cbd277cf34116f634492d40596647c9
Verseillie Yoann <yoann.verseillie@gmail.com>
2020-03-04 10:39:00 +0100

    First version of House Supervision

---
+++ addcomputer.php
@@ -0,0 +1,28 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "house_super");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$ip = mysqli_real_escape_string($link, $_REQUEST['ip']);
+$name = mysqli_real_escape_string($link, $_REQUEST['name']);
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO pc_list (ip, name) VALUES ('$ip', '$name')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
+header('Location: index.php');
+
+?>
---
+++ removeComputer.php
@@ -0,0 +1,27 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "house_super");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$id = $_GET['rm'];
+ 
+// Attempt insert query execution
+$sql = "DELETE FROM pc_list WHERE id = '$id'";
+if(mysqli_query($link, $sql)){
+    echo "Records removed successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
+header('Location: index.php');
+
+?>
