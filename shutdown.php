1fa463564e122f90aa81e84c94e12799a96ffa5f
Verseillie Yoann <yoann.verseillie@gmail.com>
2020-05-16 12:49:49 +0200

    add shutdown system

--- shutdown.php
+++ shutdown.php
@@ -5,15 +5,14 @@
 include('include/dbconnect.php');
  
 // Escape user inputs for security
-$username = $_GET['usern'];
-$password = $_GET['psswd'];
+$username = mysqli_real_escape_string($db, $_REQUEST['usern']);
+$password = mysqli_real_escape_string($db, $_REQUEST['psswd']);
 if(isset($_COOKIE["shut"])){ 
 $ip = $_COOKIE["shut"];
 }
 //Execution du script python. Trame bien envoyée (confirmation wireshark)
 
 exec('python shutdown.py '. $username . ' ' . $password . ' ' . $ip);
-
 // Close connection
 mysqli_close($db);
 
